71d1883026036270c76e0846b3fec9a297c4c192
dwioopz <dr.dwioopz@yahoo.com>
2015-06-26 14:01:54 +0700

    Update Server Running

--- Touchpad/Server.cpp
+++ Touchpad/Server.cpp
@@ -6,3 +6,52 @@ using namespace ts;
 
 // Map android keycode to VK.
 UINT MapKeycode(ANDROID_KEYCODE keycode);
+
+bool Server::IsRunning()
+{
+	return server.IsValid() && Thread::IsRunning();
+}
+
+bool Server::Run(short port, int password)
+{
+	Thread::Stop();	
+
+	// Close sockets.
+	client.Close();
+	server.Close();
+	for(int i = 0; i < 2; ++i)
+		beacons[i].Close();
+	
+	try
+	{
+		// Listen for clients.
+		server.Listen(port, 3, false);
+		
+		// Bind beacons.
+		try { beacons[0].Bind(DefaultPort, false); } 
+		catch(socket_exception & ex) { Log(OL_ERROR, L"%S", ex.what()); }
+		if(port != DefaultPort)
+		{
+			try { beacons[1].Bind(port, false); } 
+			catch(socket_exception & ex) { Log(OL_ERROR, L"%S", ex.what()); }
+		}
+
+		this->port = port;
+		this->password = password;
+
+		Thread::Run();
+
+		std::wstring host = Address::LocalHost(port).ToString();
+		Log(OL_NOTIFY | OL_STATUS | OL_INFO, L"Server running at %s\r\n", host.c_str());
+		return true;
+	}
+	catch(socket_exception & ex)
+	{
+		Log(OL_ERROR, L"%S", ex.what());
+
+		server.Close();
+
+		Log(OL_NOTIFY | OL_STATUS | OL_ERROR, L"Error initializing server on port %i!\r\n", port);
+		return false;
+	}
+}
