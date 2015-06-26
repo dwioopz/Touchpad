62240a0a4a75c40a7916f125ff203ec2058dfb2f
dwioopz <dr.dwioopz@yahoo.com>
2015-06-26 15:34:55 +0700

    Update header files

---
+++ Touchpad/resource.h
@@ -0,0 +1,59 @@
+/ / { { N O _ D E P E N D E N C I E S } } 
+ 
+ / /   M i c r o s o f t   V i s u a l   C + +   g e n e r a t e d   i n c l u d e   f i l e . 
+ 
+ / /   U s e d   b y   S e r v e r . r c 
+ 
+ / / 
+ 
+ # d   I D D _ D I A L O G                                             1 0 1 
+ 
+ # d e f i n e   I D I _ I C O N                                                 1 0 2 
+ 
+ # d e f i n e   I D R _ M E N U                                                 1 0 3 
+ 
+ # d e f i n e   I D I _ B A D                                                   1 0 4 
+ 
+ # d e f i n e   I D C _ L O G                                                   1 0 0 1 
+ 
+ # d e f i n e   I D C _ S H O W L O G                                           1 0 0 2 
+ 
+ # d e f i n e   I D C _ P O R T                                                 1 0 0 3 
+ 
+ # d e f i n e   I D C _ P A S S W O R D 1                                       1 0 0 4 
+ 
+ # d e f i n e   I D C _ P O R T 3                                               1 0 0 5 
+ 
+ # d e f i n e   I D C _ P A S S W O R D 2                                       1 0 0 5 
+ 
+ # d e f i n e   I D C _ S T A T U S                                             1 0 0 6 
+ 
+ # d e f i n e   I D _ C O N T E X T _ E X I T                                   4 0 0 0 1 
+ 
+ # d e f i n e   I D _ C O N T E X T _ S E T T I N G S                           4 0 0 0 2 
+ 
+ # d e f i n e   I D _ C O N T E X T _ R E S T A R T S E R V E R                 4 0 0 0 3 
+ 
+ 
+ 
+ / /   N e x t   d e f a u l t   v a l u e s   f o r   n e w   o b j e c t s 
+ 
+ / /   
+ 
+ # i f d e f   A P S T U D I O _ I N V O K E D 
+ 
+ # i f n d e f   A P S T U D I O _ R E A D O N L Y _ S Y M B O L S 
+ 
+ # d e f i n e   _ A P S _ N E X T _ R E S O U R C E _ V A L U E                 1 0 5 
+ 
+ # d e f i n e   _ A P S _ N E X T _ C O M M A N D _ V A L U E                   4 0 0 0 4 
+ 
+ # d e f i n e   _ A P S _ N E X T _ C O N T R O L _ V A L U E                   1 0 0 7 
+ 
+ # d e f i n e   _ A P S _ N E X T _ S Y M E D _ V A L U E                       1 0 1 
+ 
+ # e n d i f 
+ 
+ # e n d i f 
+ 
+ 
