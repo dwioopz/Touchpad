50b4f655dfd4272198b0ce9f0b43168833e71ce4
dwioopz <dr.dwioopz@yahoo.com>
2015-06-26 13:28:25 +0700

    Update Main.cpp

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -3,4 +3,31 @@
 #include "resource.h"
 
 using namespace ts;
+#include "Server.h"
+
+#include "resource.h"
+
+using namespace ts;
+
+// konstanta
+const wchar_t * RegistryKey = L"Software\\Things & Stuff\\Touchpad Server";
+const OUTPUT_LEVEL OutputLevel = OL_INFO;
+
+// notif
+NOTIFYICONDATA Notify;
+// Port.
+int Port = DefaultPort;
+
+// Password hash
+int Password = 0;
+
+// Server thread.
+Server server;
+
+// Log & message
+HWND LogWnd = NULL;
+HWND StatusWnd = NULL;
+
+// Compute hash of string.
+int Hash(const wchar_t * str);
 
